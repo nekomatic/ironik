51d36890888549b1d0b159b504d5080ad0826019
Michal Pawluk <mike@nekomatic.com>
2018-03-16 20:10:25 +0000

    Added Exclude ( anythingBut )

---
+++ src/main/kotlin/com/nekomatic/ironik/core/combinators/Exclude.kt
@@ -0,0 +1,21 @@
+package com.nekomatic.ironik.core.combinators
+
+import com.nekomatic.ironik.core.IInput
+import com.nekomatic.ironik.core.ParserResult
+import com.nekomatic.ironik.core.parsers.Parser
+
+fun <T : Any, TStreamItem : Any> anythingBut(parser: Parser<T, TStreamItem>): Parser<T, TStreamItem> {
+    val name = "Anything which is not ${parser.name}"
+    return Parser(
+            name = name,
+            parseFunction = fun(input: IInput<TStreamItem>): ParserResult<TStreamItem, TStreamItem> {
+                val resultA = parser.parse(input)
+                return if (resultA is ParserResult.Success)
+                    ParserResult.Failure(
+                            position = input.position,
+                            expected = "Anything which is not " + resultA.payload.joinToString { it.toString() }
+                    )
+                else
+                    any<TStreamItem>().parse(input)
+            })
+}
